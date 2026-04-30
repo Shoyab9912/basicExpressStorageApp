014b8d7341924c1f4e572468597b5b58477c0203
deadly9912 <mashoyab703@gmail.com>
2026-04-30 14:50:02 +0530

    add sharelink in file and directory model

--- models/directory.model.js
+++ models/directory.model.js
@@ -1,29 +1,33 @@
-import { Schema, model } from 'mongoose'
-
+import { Schema, model } from "mongoose";
+import { permission } from "node:process";
 
 const directorySchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "name field required"],
+  name: {
+    type: String,
+    required: [true, "name field required"],
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  parentDirId: {
+    type: Schema.Types.ObjectId,
+    ref: "Directory",
+    default: null,
+  },
+  sharedWith: [
+    {
+      userId: { type: Schema.Types.ObjectId, ref: "User" },
+      permission: { type: String, enum: ["viewer", "editor"] },
     },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
-    parentDirId: {
-        type: Schema.Types.ObjectId,
-        ref: "Directory",
-        default: null
-    },
-    sharedWith: [
-        {
-            userId: { type: Schema.Types.ObjectId, ref: "User" },
-            permission: { type: String, enum: ["viewer", "editor"] }
-        }
-    ]
-})
+  ],
+  shareLink: {
+    token: { type: String, default: null },
+    permission: { type: String, enum: ["viewer", "editor"] },
+    expiresAt: { type: Date, default: null },
+  },
+});
 
+const Directory = model("Directory", directorySchema);
 
-const Directory = model("Directory", directorySchema)
-
-export default Directory
+export default Directory;
